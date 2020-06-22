ebb9ac394d276c92a08fb5f035fbd171286fd19d
Christie Ryan <cryan11@cnm.edu>
2020-06-22 13:02:30 -0600

    added trick class

--- src/edu/cnm/deepdive/Suit.java
+++ src/edu/cnm/deepdive/Suit.java
@@ -8,12 +8,19 @@ package edu.cnm.deepdive;
  */
 public enum Suit {
 
-  CLUBS, DIAMONDS, HEARTS, SPADES;
+  CLUBS(Color.BLACK),
+  DIAMONDS(Color.RED),
+  HEARTS(Color.RED),
+  SPADES(Color.BLACK);
+
+  private final Color color;
+
+  Suit(Color color) {
+    this.color = color;
+  }
 
   /**
    * Returns the <em>Unicode</em> symbol for the current instance.
-   *
-   * @return One of the four symbols: ♠;, ♡;, ♢;, ♣;
    */
   public char symbol() {
     switch (this) {
@@ -29,4 +36,7 @@ public enum Suit {
     }
   }
 
+  public Color getColor() {
+    return color;
   }
+}
